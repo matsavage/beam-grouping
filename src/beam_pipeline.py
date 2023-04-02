5075fbd202e30b8b111b54ad88a49573b891ec3d
Mathew Savage <mathew@mathewsavage.com>
2023-04-02 22:28:45 +0100

    added dataclasses

--- src/beam_pipeline.py
+++ src/beam_pipeline.py
@@ -3,15 +3,40 @@ import logging
 import json
 
 from datetime import datetime
+from collections import namedtuple
 
 import apache_beam as beam
-from apache_beam.io import ReadFromPubSub
+from apache_beam import window
+from apache_beam.io import ReadFromPubSub, WriteToBigQuery
 from apache_beam.options.pipeline_options import PipelineOptions
+
+InputElement = namedtuple("InputElement", "id timestamp")
 
 
 class ExtractElement(beam.DoFn):
     def process(self, element, *args, **kwargs):
-        return [(element["id"], element)]
+        try:
+            element_json = json.loads(element.decode("utf-8"))
+
+            output_element = InputElement(
+                id=element_json["id"],
+                timestamp=datetime.fromisoformat(element_json["timestamp"]),
+            )
+
+            yield beam.pvalue.TaggedOutput(
+                "InputElement",
+                (
+                    output_element.id,
+                    # output_element
+                    window.TimestampedValue(
+                        output_element,
+                        output_element.timestamp.timestamp(),
+                    ),
+                ),
+            )
+
+        except Exception as exception:
+            yield beam.pvalue.TaggedOutput("Exception", [(element, exception)])
 
 
 class Logger(beam.DoFn):
@@ -39,22 +64,23 @@ def main(argv=None, save_main_session=True):
             >> ReadFromPubSub(
                 subscription="projects/ms-data-projects/subscriptions/demo-topic-sub"
             )
-            | "To Json" >> beam.Map(lambda e: json.loads(e.decode("utf-8")))
-            | "Timestamp elements"
-            >> beam.Map(
-                lambda elem: beam.window.TimestampedValue(
-                    elem, datetime.fromisoformat(elem["timestamp"]).timestamp()
-                )
-            )
-            | "Unpack" >> beam.ParDo(ExtractElement())
+            | "Unpack Message"
+            >> beam.ParDo(ExtractElement()).with_outputs("InputElement", "Exception")
         )
 
         windows = (
-            input
+            input.InputElement
             | "GroupIntoBatches"
             >> beam.GroupIntoBatches(batch_size=100, max_buffering_duration_secs=1)
             | "LogWindow" >> beam.ParDo(Logger(label="raw_window"))
         )
+
+        # all_exceptions = beam.Flatten(
+        #     input.Exception,
+        #     ...
+        # )
+
+        # all_exceptions >> WriteToBigQuery(...)
 
 
 if __name__ == "__main__":
